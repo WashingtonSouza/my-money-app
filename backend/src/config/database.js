c9a64ccbc83b2ae3c6f74f7d6f4ba9e56b225023
Washington Souza <thewashington.souza@gmail.com>
2019-10-15 18:38:27 -0300

    Error messages implementation

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "The '{PATH}' attribute is required."
+mongoose.Error.messages.Number.min = "The {VALUE} is less than the {MIN} informed limit"
+mongoose.Error.messages.Number.max = "The {VALUE} is greater than the {MAX} informed limit"
+mongoose.Error.messages.String.enum = "The {VALUE} is not valid for {PATH} attribute"
