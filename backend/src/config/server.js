7848476be14b90da498ecaae3dcab0c6dd2758e2
Washington Souza <thewashington.souza@gmail.com>
2019-10-15 21:30:46 -0300

    Cors implementation

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,10 +1,12 @@
 const port = 3003,
     bodyParser = require('body-parser'),
     express = require('express'),
-    server = express()
+    server = express(),
+    allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}`)
