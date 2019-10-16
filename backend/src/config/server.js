cc58d01ec18a8b9462f8db682d4c1ad8a033f4f4
Washington Souza <thewashington.souza@gmail.com>
2019-10-15 21:42:47 -0300

    Add middleware 'express-query-int'

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,11 +2,13 @@ const port = 3003,
     bodyParser = require('body-parser'),
     express = require('express'),
     server = express(),
-    allowCors = require('./cors')
+    allowCors = require('./cors'),
+    queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}`)
