f2eb5e8d83918204b6f11ae069a262062a19c3e9
Washington Souza <thewashington.souza@gmail.com>
2019-11-03 00:34:03 -0300

    BillingCycleActions and BillingCycleReducer creation

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducers'
+import BillingCycleReduer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCyle: BillingCycleReduer
 })
 
 export default rootReducer
