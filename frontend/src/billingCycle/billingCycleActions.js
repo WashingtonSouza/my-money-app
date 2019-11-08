d9583c92d69a9b030577469dfe0b7513c0c1ae83
Washington Souza <thewashington.souza@gmail.com>
2019-11-07 23:30:47 -0300

    Showing toast Error and Success messages

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 
 const BASE_URL ='http://localhost:3003/api'
 
@@ -12,6 +13,13 @@ export function getList() {
 
 export function create(values) {
     axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Success', 'Operation successfully performed')
+        })
+        .catch(e => {
+            e.response.data.errors.forEach(error => toastr.error('Error', error))
+        })
+
     return {
         type: 'TEMP'
     }
