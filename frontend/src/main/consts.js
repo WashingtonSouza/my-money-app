000a674e68771a4433028bae8f5e59e66b5c8c04
Washington Souza <thewashington.souza@gmail.com>
2019-12-29 01:06:09 -0300

    Navbar auth creation

---
+++ frontend/src/main/consts.js
@@ -0,0 +1,8 @@
+const baseUrl = 'http://localhost:3003'
+
+export default {
+    API_URL: `${baseUrl}/api`,
+    OAPI_URL: `${baseUrl}/oapi`,
+    TOKEN_VALIDATED: 'TOKEN_VALIDATED',
+    USER_FETCHED: 'USER_FETCHED'
+}
